d5963b38cd8015fab6ed87f31e2a5fc29d470d9d
Minjae Jo <107386533+minjae46@users.noreply.github.com>
2022-10-04 20:40:24 +0900

    [unrated] Title: 옷가게 할인 받기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,13 @@
+function solution(price) {
+    var answer = 0;
+    if (price >= 0 && price < 100000) {
+        answer = price;
+    } else if (price >= 100000 && price < 300000) {
+        answer = price*0.95;
+    } else if (price >= 300000 && price < 500000) {
+        answer = price*0.90;
+    } else if (price >= 500000 && price <= 1000000) {
+        answer = price*0.8;
+    } 
+    return Math.floor(answer);
+}
