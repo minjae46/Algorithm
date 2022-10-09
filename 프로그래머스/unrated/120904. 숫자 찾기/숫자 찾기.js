58a1108114daea0fee41a5e187f63b888b3e0dfc
Minjae Jo <107386533+minjae46@users.noreply.github.com>
2022-10-09 17:22:06 +0900

    [unrated] Title: 숫자 찾기, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120904. 숫자 찾기/숫자 찾기.js
@@ -0,0 +1,9 @@
+function solution(num, k) {
+    let numArr = num.toString().split('')
+    let Idx = numArr.findIndex(el => parseInt(el) === k)
+    if (Idx === -1) {
+        return Idx
+    } else {
+    return Idx + 1 
+    }
+}
