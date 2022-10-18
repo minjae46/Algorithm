30636be99debf3f7dd09e8f15d2d85276f097a0f
Minjae Jo <107386533+minjae46@users.noreply.github.com>
2022-10-18 16:25:46 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -0,0 +1,14 @@
+function solution(numbers, direction) {
+    let firstNum = numbers[0]
+    let lastNum = numbers[(numbers.length)-1]
+    if (direction === "right") {
+        numbers.pop(lastNum)
+        numbers.unshift(lastNum)
+        return numbers
+    }
+    if (direction === "left") {
+        numbers.push(firstNum)
+        numbers.shift()
+        return numbers
+    }
+}
