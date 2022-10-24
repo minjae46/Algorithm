5e4a89f394a48da25f307fd88ca2ecf6005f2d94
Minjae Jo <107386533+minjae46@users.noreply.github.com>
2022-10-24 12:38:39 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -0,0 +1,7 @@
+function solution(numbers) {
+    const arr = [0,1,2,3,4,5,6,7,8,9]
+    let result = arr.filter(el => !numbers.includes(el))
+    let answer = result.reduce((a,b) => a+b)
+    return answer
+    
+}
