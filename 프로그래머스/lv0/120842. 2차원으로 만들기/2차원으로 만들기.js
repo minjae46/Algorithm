09426bcc27514e84fe26db8de23bdbf9f6af35b5
Minjae Jo <107386533+minjae46@users.noreply.github.com>
2022-10-25 16:16:42 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,20 +1,16 @@
-// function solution(num_list, n) {
+function solution(num_list, n) {
 //     const row = num_list.length/n
 //     const column = n 
     
 //     let arr = new Array(row);
-    
 //     for (let i=0; i<row; i++) {
 //         arr[i] = new Array(column)
-//         console.log(arr)
-    
 //     }
-// }
-
-function solution(num_list, n) {
-  let result = [];
-  for (let i = 0; i < num_list.length / n; i++) {
-    result = [...result, num_list.slice(i * n, i * n + n)];
-  }
-  return result;
+    // 새 2차원 배열 생성 방법 
+    
+    let arr = [];
+    for (let i=0; i<num_list.length; i=i+n) {
+        arr.push(num_list.slice(i,i+n))
+    }
+    return arr;
 }
