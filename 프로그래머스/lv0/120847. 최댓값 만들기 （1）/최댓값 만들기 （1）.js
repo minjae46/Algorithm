bc7518ccb8383ad5ecbfa22971b98aae14979447
Minjae Jo <107386533+minjae46@users.noreply.github.com>
2022-10-25 21:06:38 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -0,0 +1,7 @@
+function solution(numbers) {
+    let answer = numbers.sort((a,b) => {
+       return a-b
+    });
+    answer = answer[answer.length-1]*answer[answer.length-2];
+    return answer;
+}
