834a764bc50b21a9aec74dbe4a4a660693c4549d
Minjae Jo <107386533+minjae46@users.noreply.github.com>
2022-10-25 21:17:14 +0900

    [level 0] Title: 7의 개수, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
@@ -0,0 +1,13 @@
+function solution(array) {
+    const newArr = [...array].join('')
+    const newNewArr = newArr.split('')
+    let count = 0;
+    
+    for (let i in newNewArr) {
+        if (newNewArr[i] === '7') {
+            count++; 
+        } 
+    }
+    
+    return count;
+}
