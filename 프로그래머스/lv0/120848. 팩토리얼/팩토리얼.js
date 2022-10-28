52c65cc686a7709d9665f383cb7d8679f9fec348
Minjae Jo <107386533+minjae46@users.noreply.github.com>
2022-10-28 19:10:20 +0900

    [level 0] Title: 팩토리얼, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
@@ -1,15 +1,28 @@
 function solution(n) {
     let result = 1;
+    let i = 0;
     let count = 0;
     
-    for (let i=1; i<=10; i++) {
-        result *= i
-        console.log(result)
-        if (result > n) {
-            return i-1
-        } else if (result !== 1 && result === n) {
-            return i
-        }
+    while (result < n) {
+        i++;
+        result = result * i
+        count++;
     }
-
+    if (result > n) {
+        return count-1
+    } else if (result !== 1 && result === n) {
+        return count
+    } else {
+        return 1
+    }
+    
+    
+    // for (let i=1; i<=10; i++) {
+    //     result *= i
+    //     if (result > n) {
+    //         return i-1
+    //     } else if (result !== 1 && result === n) {
+    //         return i
+    //     }
+    // }
 }
