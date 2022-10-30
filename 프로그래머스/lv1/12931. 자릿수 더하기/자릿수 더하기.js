e8045bf5075702ecc34af879aa956f6633427dbd
Minjae Jo <107386533+minjae46@users.noreply.github.com>
2022-10-30 20:22:34 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,6 +1,6 @@
 function solution(n) {
-    const nArr = n.toString().split('');
-    const nNumArr = nArr.map(el => {return parseInt(el)})
+    const nStrArr = n.toString().split('');
+    const nNumArr = nStrArr.map(el => {return parseInt(el)})
     const result = nNumArr.reduce((acc, cur) => {return acc + cur});
     return result;
 }
