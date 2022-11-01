cd20009e365b7afc6dd1a00d2bb3d95d8d8fcb15
Minjae Jo <107386533+minjae46@users.noreply.github.com>
2022-11-01 15:46:03 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -2,14 +2,14 @@ function solution(s) {
     if (s.length !== 4 && s.length !== 6) {
         return false;
     } else {
-        console.log("두번째", s)
-        console.log(Number(s.toString()))
         if (isNaN(s)) {
             return false;        
         } else if (s.split('').includes('e')) {
             return false;
+        // 지수 형태가 오는 경우 대비 
         } else {
             return true;
         }
     }
+    // 완전히 거르려면 정규식 쓰는 방법밖에 없는 듯. 
 }
