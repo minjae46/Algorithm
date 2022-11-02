56e3f8eafbc77a52c58bf076ab6e740e14408c5e
Minjae Jo <107386533+minjae46@users.noreply.github.com>
2022-11-02 15:53:16 +0900

    [level 0] Title: 가까운 수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -1,26 +1,19 @@
 function solution(array, n) {
     const diffArr = array.map(el => Math.abs(n-el))
-    console.log(diffArr)
     const min = Math.min(...diffArr)
-    console.log("최소차", min)
+    // 모든 요소와 n의 차이를 구해 비교한 뒤, 가장 작은 차이의 수를 구한다 
     
     let idxArr = [];
-
     for (let i=0; i<diffArr.length; i++) {
         if (diffArr[i] === min) {
             idxArr.push(i)      
         }
     }
-    console.log("인덱스배열", idxArr)
-    
+    // 최소차이 수의 인덱스를 구하는 함수 
+    // 최소차이 수가 여러개, 즉 가까운 수가 여러 개 일 경우 포함
+
     const resultArr = idxArr.map(el => array[el])
-    console.log("결과배열", resultArr)
-    
     const answer = Math.min(...resultArr)
-    console.log(answer)
     
-    // const minIdx = diffArr.indexOf(min)
-    // console.log(minIdx)
-    // const answer = array[minIdx]
-    return answer
+    return answer;
 }
