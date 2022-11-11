cb77fe55fd6ed1ab2c963832e0e8791da5dc61f6
Minjae Jo <107386533+minjae46@users.noreply.github.com>
2022-11-11 20:58:12 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -2,5 +2,5 @@ function solution(my_string) {
     const strArr = my_string.split('');
     let answer = strArr.filter(str => parseInt(str));
     answer = answer.reduce((a,b)=> parseInt(a)+parseInt(b));
-    return answer
+    return answer;
 }
