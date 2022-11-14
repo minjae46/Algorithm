ba809c71d15460e5ed0e5200d7c21eaa54d33dd8
Minjae Jo <107386533+minjae46@users.noreply.github.com>
2022-11-14 20:36:39 +0900

    [level 0] Title: 공 던지기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
@@ -1,12 +1,6 @@
-function solution(numbers, k) {
-    // if (k === 1) {
-    //     return numbers[0];
-    // }
-    
+function solution(numbers, k) {  
     const Idx = (k-1)*2;
-    console.log(Idx)
-    console.log(numbers.length)
     const drow = Math.abs(Idx%numbers.length);
-    const answer = numbers[drow]
+    const answer = numbers[drow];
     return answer;
 }
