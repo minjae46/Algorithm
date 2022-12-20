492813caa17a84d20b95b4aa5d126d1a0dd155a5
Minjae Jo <107386533+minjae46@users.noreply.github.com>
2022-12-20 23:54:59 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.js
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -1,4 +1,11 @@
 function solution(num1, num2) {
     const answer = num1 === num2 ? 1 : -1 
+    /* 
+    if (num1 === num2) {
+        return 1;  
+    } else {
+        return -1;
+    }
+    */
     return answer;
 }
