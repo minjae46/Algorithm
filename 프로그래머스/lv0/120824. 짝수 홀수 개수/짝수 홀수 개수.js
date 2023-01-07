794d71014cae6e82bc0bdb371bed722bc183b378
Minjae Jo <107386533+minjae46@users.noreply.github.com>
2023-01-07 22:49:14 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,11 +1,11 @@
 function solution(num_list) {
     let even = [];
     let odd = [];
-    for (let i in num_list) {
+    for(let i in num_list) {
         if (num_list[i]%2 === 0) {
-            even.push(num_list[i])
+            even.push(num_list[i]);
         } else {
-            odd.push(num_list[i])
+            odd.push(num_list[i]);
         }
     }
     return [even.length, odd.length];
