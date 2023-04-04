e0b6eb9ca230a2726fcf3d34e0bc88b26539f604
Minjae Jo <107386533+minjae46@users.noreply.github.com>
2023-04-04 17:23:01 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
@@ -2,11 +2,11 @@ function solution(numbers) {
     let answer;
     const sum = numbers.reduce((acc, cur) => {return acc+cur});
     answer = sum/numbers.length;
+    return answer;
     /*     
     for(i of numbers) {
         answer += i
     }
     return answer/numbers.length;
     */
-    return answer;
 }
