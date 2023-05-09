594108242327fbd3d2d8670e44a604fbd2b2287c
Minjae Jo <107386533+minjae46@users.noreply.github.com>
2023-05-09 16:45:17 +0900

    [level 0] Title: 몫 구하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120805. 몫 구하기/몫 구하기.js
+++ 프로그래머스/lv0/120805. 몫 구하기/몫 구하기.js
@@ -1,5 +1,4 @@
 function solution(num1, num2) {
-    //const answer = Math.floor(num1/num2);
-    const answer = parseInt(num1/num2);
+    const answer = Math.floor(num1/num2);
     return answer;
 }
