2afc4a39b9531ab4839ef077a5f8fe30bee53d27
Minjae Jo <107386533+minjae46@users.noreply.github.com>
2023-07-19 17:49:49 +0900

    [unrated] Title: 수 조작하기 1, Time: 4.53 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
@@ -2,13 +2,13 @@ function solution(n, control) {
     let answer = 0;
     for(let i=0; i<control.length; i++) {
         if(control[i] === "w") {
-            n = n+1;
+            n++ ;
         } else if(control[i] === "s") {
-            n = n-1;
+            n-- ;
         } else if(control[i] === "d") {
-            n = n+10;
+            n += 10;
         } else if(control[i] === "a") {
-            n = n-10;
+            n -= 10;
         }
     }
     answer = n;
