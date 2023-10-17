ef13fa729639180210ddb8a2bd3da6525e98a037
Minjae Jo <107386533+minjae46@users.noreply.github.com>
2023-10-17 17:30:57 +0900

    [level unrated] Title: 코드 처리하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.js
@@ -0,0 +1,23 @@
+function solution(code) {
+    let answer = '';
+    let mode = 0;
+    for(let i=0; i<code.length; i++) {
+        if(mode === 0) {
+            if(code[i] !== "1" && i%2 === 0) {
+                 answer += code[i];
+            } else if(code[i] === "1") {
+                mode = 1;
+            }
+        }
+        else if(mode === 1) {
+            if(code[i] !== "1" && i%2 === 1) {
+                answer += code[i];
+            } else if(code[i] === "1") {
+                mode = 0;
+            }
+        }
+    }
+    
+    if(answer === "") return "EMPTY";
+    else return answer;
+}
