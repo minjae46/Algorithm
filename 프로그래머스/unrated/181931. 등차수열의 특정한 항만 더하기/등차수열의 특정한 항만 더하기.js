630dc3edf8a66ae6d345e2c615e65f35003fd434
Minjae Jo <107386533+minjae46@users.noreply.github.com>
2024-01-15 19:24:16 +0900

    [level unrated] Title: 등차수열의 특정한 항만 더하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
+++ 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
@@ -1,13 +1,9 @@
 function solution(a, d, included) {
     let answer = 0;
     let sum = 0;
-    function getValue(i) {
-        if(i === 0) return a;
-        else return a+(d*i);
-    }
     for(let i=0; i<included.length; i++) {
         if(included[i] == true) {
-            sum += getValue(i);       
+            sum += a+(d*i);     
         }
     answer = sum;
     }
