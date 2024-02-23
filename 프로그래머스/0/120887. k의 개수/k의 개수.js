d80d48c9dfef34299e1d3d9afcfab4626e3386da
Minjae Jo <107386533+minjae46@users.noreply.github.com>
2024-02-23 15:44:22 +0900

    [level 0] Title: k의 개수, Time: 19.09 ms, Memory: 42.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120887. k의 개수/k의 개수.js
@@ -0,0 +1,22 @@
+function solution(i, j, k) {
+    let numArr = [];
+    for (let l=i; l<=j; l++) {
+        numArr.push(l)
+    }
+    //i부터 j까지의 숫자 배열을 만든다
+    
+    let allArr = [];
+    for (let m=0; m<numArr.length; m++) {
+        let strNum = numArr[m].toString().split('')
+        allArr.push(...strNum)
+    }
+    //숫자 배열 요소의 모든 각 자릿수가 문자로 담긴 배열을 만든다
+    
+    let count = 0;
+    for (let o of allArr) {
+        if (o === k.toString()) {
+            count++;
+        }
+    }
+    return count;
+}
