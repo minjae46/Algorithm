2b331fbb1a38d8cbe8579433ab7a9b7649ed579f
Minjae Jo <107386533+minjae46@users.noreply.github.com>
2024-04-02 21:49:20 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -0,0 +1,8 @@
+function solution(sides) {
+    const newArr = sides.sort((a,b) => {return a-b});
+    if(newArr[0] + newArr[1] > newArr[2]) {
+        return 1;
+    } else {
+        return 2;
+    }
+}
