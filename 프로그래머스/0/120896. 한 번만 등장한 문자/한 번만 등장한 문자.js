e6e7a2b5311d827dbc60efa1bcda56d1385060b6
Minjae Jo <107386533+minjae46@users.noreply.github.com>
2024-04-08 18:42:10 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -0,0 +1,19 @@
+function solution(s) {
+    let obj = {};
+    
+    for(let i=0; i<s.length; i++) {
+        let letter = s[i];
+        obj[letter] ? obj[letter] += 1 : obj[letter] = 1
+    }
+    //객체로 빈도 수 세기 패턴 이용
+    
+    const resultArr = [];
+    for(let i in obj) {
+        if (obj[i] === 1) {
+            resultArr.push(i)
+        }
+    }
+
+    const answer = resultArr.sort().join('')
+    return answer
+}
